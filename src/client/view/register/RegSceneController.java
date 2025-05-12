4c0eac7d31e2bfb8e7e8181dc96a6607e3b2acca
InfernumVII <egor554shved@gmail.com>
2025-05-12 20:46:48 +0300

    copy login scene to regscene

---
+++ src/client/view/register/RegSceneController.java
@@ -0,0 +1,36 @@
+package client.view.register;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class RegSceneController implements Initializable {
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Text signUp;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println(username);
+    }
+
+    public void signUpMouseEntered(){
+        signUp.setStrokeWidth(0.1);
+    }
+
+    public void signUpMouseExited(){
+        signUp.setStrokeWidth(0);
+    }
+
+    public void signUpClicked(){
+        
+    }
+}
